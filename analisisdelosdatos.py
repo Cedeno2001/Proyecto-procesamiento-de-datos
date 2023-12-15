6d2545a6b03971aa103e31f851ac880f4bc460fc
Victoria Cedeño <genecede2001@gmail.com>
2023-12-15 22:51:19 +0100

    parte_3

--- analisisdelosdatos.py
+++ analisisdelosdatos.py
@@ -14,3 +14,14 @@ promedio_edad_no_fallecidas = personas_no_fallecidas["age"].mean()
 
 print(f"promedio de edad de las personas fallecidas: {promedio_edad_fallecidos}")
 print(f"promedio de edad de las personas no fallecida: {promedio_edad_no_fallecidas}")
+
+tipos_de_datos = df.dtypes
+print("Tipos de datos:")
+print(tipos_de_datos)
+
+if 'is_male' in df.columns and 'is_smoker' in df.columns:
+    fumadores_por_genero = df.groupby(['is_male', 'is_smoker']).size().unstack()
+    print("\ncantidad de hombres y mujeres fumadores:")
+    print(fumadores_por_genero)
+else:
+    print("las columnas'is_male' y 'is_smoker' no estan presentes en el dataframe.")
